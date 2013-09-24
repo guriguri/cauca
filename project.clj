735c36158e7b667b1e5f0407a254d5f61f4172d7
guriguri <guriguri@synergetics.co.kr>
2013-09-24 15:06:50 +0900

    1. modified .gitignore, project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (def VERSION (.trim (slurp "VERSION")))
-(def MODULES (-> "MODULES" slurp (.split "\n")))
+(def MODULES (for [m (.split (slurp "MODULES") "\newline")](.trim m)))
 (def DEPENDENCIES (for [m MODULES] [(symbol (str "courtauction/" m)) VERSION]))
 
-(eval `(defproject courtauction/parent ~VERSION
+(eval `(defproject courtauction ~VERSION
   :dependencies [~@DEPENDENCIES]
   :plugins [[~'lein-sub "0.2.1"]]  
   :min-lein-version "2.0.0"
