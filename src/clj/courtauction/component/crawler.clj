7ba45fb47db8efc950a7d6d64ff8fecf9bcc4928
guriguri <guriguri@synergetics.co.kr>
2013-09-27 15:23:58 +0900

    1. -ing

--- src/clj/courtauction/component/crawler.clj
+++ src/clj/courtauction/component/crawler.clj
@@ -7,11 +7,21 @@
             )
   )
 
-(defn getbyitemprop
-  "Extract node content from HTML"
-  [html key value]
-  (e/select-nodes* (e/html-snippet html)
-                   [(e/attr= key value)]))
+(defn courtauction-parser [html]
+  (def nodes (e/html-snippet (:body resp)))
+  (doseq [rows (e/select-nodes* nodes [
+                                           (e/attr= :class "Ltbl_list")
+                                           (e/tag= :tbody)
+                                           (e/tag= :tr)])]
+    (def cols (e/select-nodes* (:content rows) [(e/tag= :td)]))
+    (println (count cols) (type cols))
+    (println (get (vec cols) 0))
+    )
+;    (doseq [td (filter (comp not nil? val) (:content node))]
+;      (println "-----")
+;      (println (:content td))
+;      )
+  )
 
 (defn get-list! [sido sigu]
   (do
@@ -20,73 +30,46 @@
                             :headers {
                                       "Host" "www.courtauction.go.kr"
                                       "User-Agent" "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.8; ko; rv:1.9.0.14) Gecko/2009082706 Firefox/3.0.14"
-                                      "Accept" "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+                                               "Accept" "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
                                       "Accept-Language" "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3"
-                                      "Accept-Encoding" "gzip,deflate"
                                       "Accept-Charset" "windows-949,utf-8;q=0.7,*;q=0.7"
-                                      "Keep-Alive" "300"
-                                      "Connection" "keep-alive"
-                                      "Referer"	"https://www.courtauction.go.kr/RetrieveMainInfo.laf"
                                       }
                             :form-params {
                                           :_CUR_CMD	"RetrieveMainInfo.laf"
                                           :_CUR_SRNID	"PNO102000"
                                           :_FORM_YN	"Y"
-                                          :_LOGOUT_CHK ""	
-                                          :_NAVI_CMD ""
-                                          :_NAVI_SRNID ""
                                           :_NEXT_CMD "RetrieveRealEstMulDetailList.laf"
                                           :_NEXT_SRNID "PNO102002"
                                           :_PRE_SRNID "PNO102001"
                                           :_SRCH_SRNID "PNO102000"
                                           :bubwLocGubun "2"
-                                          :daepyoDongCd ""
                                           :daepyoSidoCd sido
                                           :daepyoSiguCd sigu
-                                          :jibhgwanOffMgakPlcGubun ""
-                                          :jiwonNm ""
-                                          :mDaepyoDongCd ""	
                                           :mDaepyoSidoCd sido
                                           :mDaepyoSiguCd sigu
-                                          :mvDaepyoSidoCd ""
-                                          :mvDaepyoSiguCd ""
                                           :mvRealGbncd "1"
-                                          :mvmPlaceDongCd ""
-                                          :mvmPlaceSidoCd ""	
-                                          :mvmPlaceSiguCd ""
                                           :notifyLoc "1"
                                           :notifyNewLoc "1"
                                           :notifyRealRoad "1"
-                                          :rd1Cd ""
-                                          :rd2Cd ""
-                                          :rd3Rd4Cd ""
                                           :realVowel "00000_55203"
-                                          :roadCode ""
-                                          :roadPlaceSidoCd ""
-                                          :roadPlaceSiguCd ""
                                           :srnID "PNO102000"
                                           :vowelSel "00000_55203" 
                                           }
                             }
-                           )
+                             )
       )
-;    (println "XXXXX" (:content (getbyitemprop (:body resp) :class "Ltbl_list")))
-    (doseq [node (getbyitemprop (:body resp) :class "Ltbl_list")]
-      (if (= (:tag (:content node)) :tbody)
-        (println "XXXXX, " node))
-      )
-;    (println (:body resp))
-)
-  )
+    (courtauction-parser (:body resp)) 
+    )
+  ) 
 
 (defn -main
   ([]
   (log/configure-logback "/courtauction-logback.xml")
-    (config/config-yaml "/application-context.yaml") 
-    (def sido (get (. (config/get-value :location.SidoCd) split ",") 0))
+  (config/config-yaml "/application-context.yaml") 
+  (def sido (get (. (config/get-value :location.SidoCd) split ",") 0))
   (doseq [tSigu (config/get-value :location.SiguCd)]
-      (def sigu (get (. tSigu split ",") 0))
+    (def sigu (get (. tSigu split ",") 0))
     (get-list! sido sigu)
-      )
     )
+  )
   )
