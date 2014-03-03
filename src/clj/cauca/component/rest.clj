667288724f67f1f94ef9a0d4a35ffb23f89fe86e
guriguri <guriguri.kr@gmail.com>
2014-03-04 01:26:45 +0900

    1. indention 정리 2. check-validation 추가 3. 에러 등의 응답도 다 json 처리

--- src/clj/cauca/component/rest.clj
+++ src/clj/cauca/component/rest.clj
@@ -14,18 +14,8 @@
  (:gen-class)
  )
 
- (defn error-page [e]
-   (concat
-     [[:h2 "Internal Server Error"]]
-     [[:pre (let [sw (java.io.StringWriter.)]
-       (.printStackTrace e (java.io.PrintWriter. sw))
-       (.toString sw))]]
-     )
-   )
-
-
- (defn main-page []
-   (html5
+(defn main-page []
+  (html5
     [:head
      [:title "CAUCA"]
      (include-css "/css/bootstrap-1.1.0.css")
@@ -53,99 +43,85 @@
       ]
      ]
     )
-   )
+  )
 
- (defn cauca-writer [key value]
+(defn cauca-writer [key value]
   (if (or (= key :auctionDate) (= key :regDate) (= key :updDate))
     (str (java.sql.Date. (.getTime value)))
     value
     )
   )
 
- (defn response [json]
-   (log/log-message json)
-   (-> (resp/response json)
-     (resp/content-type "application/json; charset=utf-8"))
-   )
+(defn response [json status]
+  (log/log-message json)
+  (-> (resp/response json)
+    (resp/status status)
+    (resp/content-type "application/json; charset=utf-8")
+    )
+  )
+
+(defn check-validation [value min max regex isMatch error-msg]
+  (if-not (nil? value)
+    (if (false? (util/validation value min max regex isMatch))
+      (throw (Exception. error-msg))
+      )
+    )
+  )
  
- (defn check-params [params]
-   (if-not (nil? (params "page"))
-     (if (false? (util/validation (params "page") 1 3 #"[0-9]+" true))
-       (throw (Exception. "invalid.param.page"))
-       )
-     )
-   (if-not (nil? (params "pageSize"))
-     (if (false? (util/validation (params "pageSize") 1 3 #"[0-9]+" true))
-       (throw (Exception. "invalid.param.pageSize"))
-       )
-     )
-   (if-not (nil? (params "itemType"))
-     (if (false? (util/validation (params "itemType") nil nil #".*[\s~!@#$%^&*()_+`\\=\-{}|\\[\\]:\\\\\";'<>?,./].*" false))
-       (throw (Exception. "invalid.param.itemType"))
-       )
-     )
-   (if-not (nil? (params "addr0"))
-     (if (false? (util/validation (params "addr0") nil nil #".*[\s~!@#$%^&*()_+`\\=\-{}|\\[\\]:\\\\\";'<>?,./].*" false))
-       (throw (Exception. "invalid.param.addr0"))
-       )
-     )
-   (if-not (nil? (params "addr1"))
-     (if (false? (util/validation (params "addr1") nil nil #".*[\s~!@#$%^&*()_+`\\=\-{}|\\[\\]:\\\\\";'<>?,./].*" false))
-       (throw (Exception. "invalid.param.addr1"))
-       )
-     )
-   (if-not (nil? (params "auctionStartDate"))
-     (if (false? (util/validation (params "auctionStartDate") 9 11 #"20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]" true))
-       (throw (Exception. "invalid.param.auctionStartDate"))
-       )
-     )
-   (if-not (nil? (params "auctionEndDate"))
-     (if (false? (util/validation (params "auctionEndDate") 9 11 #"20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]" true))
-       (throw (Exception. "invalid.param.auctionEndDate"))
-       )
-     )
-   )
+(defn check-params [params]
+  (check-validation (params "page") 1 3 #"[0-9]+" true "invalid.param.page")
+  (check-validation (params "pageSize") 1 3 #"[0-9]+" true "invalid.param.pageSize")
+  (check-validation (params "itemType") nil nil #".*[\s~!@#$%^&*()_+`\\=\-{}|\\[\\]:\\\\\";'<>?,./].*" false "invalid.param.itemType")
+  (check-validation (params "addr0") nil nil #".*[\s~!@#$%^&*()_+`\\=\-{}|\\[\\]:\\\\\";'<>?,./].*" false "invalid.param.addr0")
+  (check-validation (params "addr1") nil nil #".*[\s~!@#$%^&*()_+`\\=\-{}|\\[\\]:\\\\\";'<>?,./].*" false "invalid.param.addr1")
+  (check-validation (params "auctionStartDate") 9 11 #"20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]" true "invalid.param.auctionStartDate")
+  (check-validation (params "auctionEndDate") 9 11 #"20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]" true "invalid.param.auctionEndDate")
+  )
    
- (defroutes main-routes
-   (GET "/" [] (main-page))
-   (GET ["/api/courtauction/:id", :id #"[0-9]+"] [id]
-        (let [dao-impl# (f/get-obj :courtauction-dao)
-              ret-courtauction (first (.get-courtauction dao-impl# id))
-              json (json/write-str ret-courtauction :value-fn cauca-writer :escape-unicode false)]
-          (response json)))
-   (GET "/api/courtauction" {params :query-params}
-        (check-params params)
-        (let [dao-impl# (f/get-obj :courtauction-dao)
-              ret-courtauction-list (.get-courtauction-list dao-impl# params)
-              json (json/write-str ret-courtauction-list :value-fn cauca-writer :escape-unicode false)]
-          (response json)))
-   (route/not-found "Page not found")
-   )
+(defroutes main-routes
+  (GET "/" [] (main-page))
+  (GET ["/api/courtauction/:id", :id #"[0-9]+"] [id]
+       (let [dao-impl# (f/get-obj :courtauction-dao)
+             ret-courtauction (first (.get-courtauction dao-impl# id))
+             json (json/write-str {:msg "ok" :result ret-courtauction}
+                                  :value-fn cauca-writer :escape-unicode false)]
+         (response json 200)))
+  (GET "/api/courtauction" {params :query-params}
+       (check-params params)
+       (let [dao-impl# (f/get-obj :courtauction-dao)
+             ret-courtauction-list (.get-courtauction-list dao-impl# params)
+             json (json/write-str {:msg "ok" :result ret-courtauction-list}
+                                  :value-fn cauca-writer :escape-unicode false)]
+         (response json 200)))
+  (route/not-found
+    (let [json (json/write-str {:msg "page.not.found"})]
+      (response json 404)))
+  )
  
- (def main-handler
-   (-> main-routes handler/api))
+(def main-handler
+  (-> main-routes handler/api))
  
- (defn catch-errors [handler]
-   (fn [request]
-     (try
-       (handler request)
-       (catch Exception e
-         (-> (resp/response (error-page e))
-           (resp/status 500)
-           (resp/content-type "text/html"))
-         ))
-     )
-   )
+(defn catch-errors [handler]
+  (fn [request]
+    (try
+      (handler request)
+      (catch Exception e
+        (let [json (json/write-str {:msg (.getMessage e)})]
+          (response json 400))
+        )
+      )
+    )
+  )
 
- (def app
-   (-> #'main-handler
-       (wrap-reload '[cauca.component.rest])
-       catch-errors))
+(def app
+  (-> #'main-handler
+    (wrap-reload '[cauca.component.rest])
+    catch-errors))
 
- (defn start-server! []
-   (run-jetty app {:port 8080 :join? false})
-   )
+(defn start-server! []
+  (run-jetty app {:port 8080 :join? false})
+  )
 
- (defn -main []
-   (start-server!)
-   )
+(defn -main []
+  (start-server!)
+  )
